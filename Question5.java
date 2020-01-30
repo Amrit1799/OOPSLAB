d1fda6e477f1a90a37e8fd391660db42f0bce5c9
Amrit1799 <45428620+Amrit1799@users.noreply.github.com>
2020-01-30 21:44:35 +0530

    Question5

---
+++ Question5.java
@@ -0,0 +1,13 @@
+class Question5
+{
+ public static void main(String args[])
+ {
+	int i=0,sum=0;
+	for(i=41;i<250;i++)
+	{
+	  if(i%5==0)
+	   sum+=i;
+	}
+        System.out.println("Sum:="+sum);
+  }
+}
